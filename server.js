6e454d1228ac2d92903e51428cdc9c0a5e724971
mamiul <kulsumeffendiA@gmail.com>
2025-02-16 15:03:55 +0700

    update 16 feb

--- server.js
+++ server.js
@@ -1,10 +1,29 @@
 const express = require("express");
 const app = express()
 const port = 3000;
+const hbs = require("hbs");
+const path = require('path')
+
+const {formatDateToWIB, getRelativeTime } = require('./utils/time')
 
 app.set("view engine", "hbs");
 
+app.set("views", path.join(__dirname, "./views")); //c:/ my computer/documents/../../progres 1/views
+//dirname itu mengacu k folder web kita
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true }));  // supaya isa ngambil data dri setiap input
+ 
 app.use(express.static("assets"));
+
+hbs.registerPartials(__dirname +"/views/partials", function (err) {});
+hbs.registerHelper("equal", function (a,b) {
+  return a === b;
+});
+hbs.registerHelper("formatDateToWIB", formatDateToWIB)
+hbs.registerHelper("getRelativeTime", getRelativeTime)
+
+let blogs = [];
 
 // HALAMAN HOME
 
@@ -12,16 +31,51 @@ app.get("/", (req, res) => {
   res.render("index"); 
 });
 
-app.get("/", (req, res) => {
-  res.render("blog"); 
+
+app.get("/blog/:id", (req, res) => {
+  res.render("blog-detail"); 
 });
 
-app.get("/", (req, res) => {
+app.get("/blog-create", (req, res) => {
+  res.render("blog-create"); 
+});
+
+app.get("/blog", (req, res) => {
+  console.log(blogs);
+  res.render("blog-list", {blogs: blogs}); 
+});
+
+
+app.get("/testimonials", (req, res) => {
   res.render("testimonials"); 
 });
 
-app.get("/", (req, res) => {
-  res.render("Contact"); 
+
+app.get("/contact", (req, res) => {
+  res.render("contact"); 
+});
+
+// SUBMIT NEW BLOG
+app.post("/blog-create", (req, res) => {
+// const title = req.body.title;
+// const content = req.body.content;
+const {title,content} = req.body; //title dan content adalah properti milik req.body
+console.log("judul adalah", title);
+console.log("contentnya:", content);
+
+let image = "https://picsum.photos/200/150"
+
+let newBlog = {
+  title: title,
+  content: content,
+  image: image,
+  author: "Amelia Kulsum E",
+  postedAt: new Date(),
+};
+
+blogs.push(newBlog);
+
+res.redirect("/blog");
 });
 
 // REQUEST PARAMS
---
+++ utils/time.js
@@ -0,0 +1,66 @@
+function formatDateToWIB(date) {
+    // let date = new Date();
+    //30 jan 2025 11:22 WIB 
+    let monthList = [
+         "Jan", // bukan 1 tapi 0 -> bkn nama bulan/angka bulannya tpi index (0-dst)
+         "Feb", 
+         "Mar", 
+         "Apr", 
+         "Mei",
+         "Jun",
+         "Jul",
+         "Aug",
+         "Sep",
+         "Okt",
+         "Nov",
+         "Des",
+        ];
+
+      let day = date.getDate().toString().padStart(2, "0");
+      let month = monthList [date.getMonth()];
+      let year = date.getFullYear();
+    }
+    function getRelativeTime (postTime) {
+        let now = new Date();
+        console.log("waktu sekarang :",now);
+    
+      console.log("waktu user post :", postTime);
+    
+      let diffTime = now - postTime;
+      console.log("selisih waktu", diffTime);
+    
+      let diffInSeconds = Math.floor( (now - postTime) / 1000);
+    
+      console.log("selisih detik", diffInSeconds);
+    
+      if(diffInSeconds < 60 ) {
+      return `${diffInSeconds} seconds ago`;
+    }
+    
+    let diffInMinutes = Math.floor(diffInSeconds / 60);
+    
+    if (diffInMinutes <60) {
+    return `${diffInMinutes} minutes ago`
+    }
+    
+    let diffInHours = Math.floor(diffInMinutes / 60);
+    
+    if (diffInHours <24) {
+        return `${diffInHours} hours ago`;
+    }
+    
+    let diffInDays = Math.floor(diffInHours / 24);
+    
+    if (diffInDays < 30);{
+        return  `${diffInDays} days ago`;
+    }
+    
+    let diffInMonth = Math.floor(diffInDays /30);
+    return `${diffInMonth} month${diffInMonth === 1 ? "": "s"} ago`;
+    }
+    
+
+    module.exports = {
+        formatDateToWIB,
+        getRelativeTime
+    };
